a9a921c531e58fe369d87b57aa24f006e771c7d0
AKdeBerg <ich.bin.anik@gmail.com>
2025-01-03 21:41:54 +0600

    redesign button

--- src/App.jsx
+++ src/App.jsx
@@ -76,7 +76,18 @@ export default function App() {
               </li>
             </ul>
 
-            <button className="m-4 bg-black text-white p-3 rounded-full">Request a demo &nbsp;&gt;</button>
+            <button className="m-4 bg-black text-white rounded-full flex items-center px-6 py-2">
+              Request a demo
+              <svg
+                className="w-4 h-4 ml-2"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" d="M9 5l7 7-7 7"></path>
+              </svg>
+            </button>
           </div>
           <div className="flex justify-center">
             <img src="/public/assets/hero.png" className="w-80" />
