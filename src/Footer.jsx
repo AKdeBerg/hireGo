da045f71064c1e27c149e8bed137a0777f6bb2ec
AKdeBerg <ich.bin.anik@gmail.com>
2025-01-08 00:37:06 +0600

    making the footer responsive

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -23,18 +23,60 @@ function Footer() {
 
       <hr className="border-t border-[#FFFFFF29]  mx-6 my-10" />
 
-      <div className="p-8 space-y-8">
-        <h2 className="text-3xl text-white">HireGo</h2>
-        <div className="flex space-x-6">
-          <img src="/public/assets/twitter.png" className="w-10 h-10" />
-          <img src="/public/assets/ln.png" className="w-10 h-10" />
-          <img src="/public/assets/fb.png" className="w-10 h-10" />
-          <img src="/public/assets/ig.png" className="w-10 h-10" />
+      <div className="mt-20 p-8 md:flex md:justify-between md:p-0 md:px-12">
+        <div className="space-y-8">
+          <h2 className="text-3xl text-white">HireGo</h2>
+          <div className="flex space-x-6">
+            <img src="/public/assets/twitter.png" className="w-10 h-10" />
+            <img src="/public/assets/ln.png" className="w-10 h-10" />
+            <img src="/public/assets/fb.png" className="w-10 h-10" />
+            <img src="/public/assets/ig.png" className="w-10 h-10" />
+          </div>
         </div>
-        <div className="flex space-x-2 items-center text-sm">
-          <img src="/public/assets/globe.png" className="w-4 h-4" />
-          <div className="text-white">English</div>
+
+        {/* hidden for mobile */}
+        <div className="hidden md:flex md:justify-between md:items-start md:space-x-8 md:pr-10">
+          <div>
+            <h3 className="text-sm font-semibold text-white pb-4">How it works</h3>
+            <ul className="text-xs text-[#FFFFFF66] space-y-2">
+              <li>Hire Employees</li>
+              <li>Hire Contractors</li>
+              <li>Run Global Payroll</li>
+              <li>App Store</li>
+              <li>Open API</li>
+            </ul>
+          </div>
+          <div>
+            <h3 className="text-sm font-semibold text-white pb-4">Solutions</h3>
+            <ul className="text-xs text-[#FFFFFF66] space-y-2">
+              <li>Compliance</li>
+              <li>Payments</li>
+              <li>For Finance Teams</li>
+              <li>For Legal Teams</li>
+              <li>Enterprise</li>
+            </ul>
+          </div>
+          <div className="pb-8">
+            <h3 className="text-sm font-semibold text-white pb-4">Resources</h3>
+            <ul className="text-xs text-[#FFFFFF66] space-y-2">
+              <li>About</li>
+              <li>Blog</li>
+              <li>Support Hub</li>
+              <li>Global Hiring Guide</li>
+              <li>Affiliates</li>
+              <li>Careers</li>
+              <li>Glossary</li>
+              <li>Case Studies</li>
+              <li>Press</li>
+              <li>Worker Community</li>
+            </ul>
+          </div>
         </div>
+      </div>
+
+      <div className="pl-8 md:pl-12 md:pb-6 flex space-x-2 items-center text-sm">
+        <img src="/public/assets/globe.png" className="w-4 h-4" />
+        <div className="text-white">English</div>
       </div>
 
       <hr className="border-t border-[#FFFFFF29]  mx-6 my-10 md:my-0" />
